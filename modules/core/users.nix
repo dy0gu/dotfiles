4ac7598183f56fbc8bc2196cb730ee59b265b840
dy0gu <dbsimoes20@gmail.com>
2025-07-01 16:41:41 +0100

    fix: reset shell default

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -5,6 +5,9 @@
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" "audio" "video" "input" ];
 
+    # Used shell must be enabled elsewhere
+    shell = pkgs.zsh;
+
     # Should be changed after first user creation
     initialPassword = "nixos";
   };
