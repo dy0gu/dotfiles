41783bef2aab455be918cb0de309b0c29d8fbdad
dy0gu <dbsimoes20@gmail.com>
2025-07-08 20:25:42 +0100

    chore: remove typo

--- modules/home-manager/dy0gu.nix
+++ modules/home-manager/dy0gu.nix
@@ -92,7 +92,7 @@
       font-family = "FiraCode Nerd Font";
       window-subtitle = false;
       window-inherit-working-directory = true;
-«      gtk-titlebar=false
+      gtk-titlebar=false
       clipboard-trim-trailing-spaces = true;
       keybind = [
         "ctrl+h=goto_split:left"
