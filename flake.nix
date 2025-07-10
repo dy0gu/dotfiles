83bad8406e8d621223b438c45c7c0115d6ea668a
dy0gu <dbsimoes20@gmail.com>
2025-07-11 00:20:38 +0100

    chore: update flake format

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
